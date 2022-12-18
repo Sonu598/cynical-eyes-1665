6716dea173a963591435151daebfb241967703c0
sonu598 <ishupriyarath@gmail.com>
2022-12-18 23:50:53 +0530

    I can not rectify and complete my code becayse of some internet issue

--- products.js
+++ products.js
@@ -1,12 +1,12 @@
-let products=document.getElementById("products");
+let products=document.getElementById("product-container");
 let fechdata=[];
-fetch("https://fakestoreapi.com/products")
+fetch("https://63987ef4044fa481d69fb790.mockapi.io/products")
 .then((response) =>{
     return response.json();
 })
 .then((acctualdata) =>{
-    fechdata=acctualdata.data;
-    displayproduct(acctualdata.data);
+    fechdata=acctualdata;
+    displayproduct(acctualdata);
 })
 .catch((error) =>{
     console.log(error);
@@ -15,12 +15,15 @@ function displayproduct(data) {
     products.innerHTML=null;
     data.forEach((element) =>{
         let card=document.createElement("div");
+        card.setAttribute("id","card");
         let img=document.createElement("img");
         img.setAttribute("src",element.avatar);
         let name=document.createElement("h4");
         name.innerText=element.name;
         let price=document.createElement("h5");
         price.innerText=element.proce;
+        let category=document.createElement("h6");
+        category.innerText=element.category;
         let description=document.createElement("p");
         description.innerText=element.description;
         let addbag=document.createElement("button");
@@ -34,16 +37,16 @@ function displayproduct(data) {
                     alreadybag=true;
                     break;
                 }
-                if (alreadybag===true) {
-                    alert("Already in Bag!");
-                } else {
-                    bagdata.push({...element,quantity:1});
-                    localStorage.setItem("bag",JSON.stringify(bagdata));
-                    alert("Added to Bag!");
-                }
             }
-            card.append(img,name,price,description,addbag);
-            products.append(card);
+            if (alreadybag===true) {
+                alert("Already in Bag!");
+            } else {
+                bagdata.push({...element,"quantity":1});
+                localStorage.setItem("bag",JSON.stringify(bagdata));
+                alert("Added to Bag!");
+            }
         })
+        card.append(img,name,price,category,description,addbag);
+        products.append(card);
     })
 }
--- signin.js
+++ signin.js
@@ -1,10 +1,10 @@
 let form=document.getElementById("signin");
 let email=document.getElementById("email");
 let button=document.getElementById("nextbtn");
+button.addEventListener("click",() =>{
 let LSdata=JSON.parse(localStorage.getItem("UserEmail")) || [];
 let obj={Useremail: form.email.value};
-button.addEventListener("click",() =>{
    LSdata.push(obj);
    localStorage.setItem("UserEmail",JSON.stringify(LSdata));
-   window.open("/signup.html")
+   window.open("/signup.html");
 });
